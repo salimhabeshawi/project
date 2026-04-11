90231e37ec58419b5cc135fe755e7cd412e6100c
salimhabeshawi <salimhabeshawi@gmail.com>
2026-04-11 23:38:16 +0300

    Add DaisyUI for enhanced styling and update layout with theme integration

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,13 +10,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>{{ $title }}</title>
-	<link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
-	<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-	<link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
+	@vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
 
-<body class="">
+<body class="text-primary">
 	<x-nav />
 
 	<main class="max-w-3xl mx-auto mt-6 mb-6">
